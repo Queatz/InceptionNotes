fef8f99605c79f58684bfc0f144f4dcecc6d81ed
Jacob Ferrero <jacobaferrero@gmail.com>
2017-06-21 21:12:22 -0500

    Drag drop to side nav

--- src/app/sub-sub-list-item/sub-sub-list-item.component.ts
+++ src/app/sub-sub-list-item/sub-sub-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 import { ApiService } from '../api.service';
 import { UiService } from '../ui.service';
@@ -6,14 +6,60 @@ import { UiService } from '../ui.service';
 @Component({
   selector: 'sub-sub-list-item',
   templateUrl: './sub-sub-list-item.component.html',
-  styleUrls: ['./sub-sub-list-item.component.css']
+  styleUrls: ['./sub-sub-list-item.component.css'],
+  host: {
+    '[style.outline]': 'isDroppingList ? \'3px solid orange\' : undefined'
+  }
 })
 export class SubSubListItemComponent implements OnInit {
   @Input() item: any;
 
+  private isDroppingList: boolean;
+  private dragCounter: number = 0;
+  
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+  }
+  
+  @HostListener('dragenter', ['$event'])
+  dragOn(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    this.dragCounter++;
+    
+    this.isDroppingList = true;
+  }
+  
+  @HostListener('dragleave', ['$event'])
+  dragOff(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    this.dragCounter--;
+    
+    // This is here to prevent flickering
+    if (this.dragCounter < 1) {
+      this.isDroppingList = false;
+    }
+  }
+  
+  @HostListener('dragover', ['$event'])
+  nothing() {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+  
+  @HostListener('drop', ['$event'])
+  drop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    this.isDroppingList = false;
+    this.dragCounter = 0;
+    let id = event.dataTransfer.getData('application/json');
+    this.api.moveList(id, this.item.id);
   }
   
   isSelectedNav(item: any) {
